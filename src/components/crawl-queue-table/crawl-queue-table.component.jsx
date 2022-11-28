649fd8a1d5d2371c43c0998512a2dc406a117537
vijay010498 <svijayr007@gmail.com>
2022-11-28 07:47:40 +0530

    add: added luxon for table

--- src/components/crawl-queue-table/crawl-queue-table.component.jsx
+++ src/components/crawl-queue-table/crawl-queue-table.component.jsx
@@ -1,5 +1,6 @@
 import {Table, Tag} from 'antd'
 import QueueStatus from "../../constants/queue-status";
+import {DateTime} from "luxon";
 
 const CrawlQueueTableComponent = ({queueJobs}) => {
   const jobs = [...queueJobs.enqueuedJobs, ...queueJobs.in_progressJobs, ...queueJobs.completedJobs, ...queueJobs.failedJobs]
@@ -40,9 +41,54 @@ const CrawlQueueTableComponent = ({queueJobs}) => {
         )
       }
     },
+    {
+      title: 'Created At',
+      dataIndex: 'created_at',
+      key: 'job_id',
+      sorter: (a, b) => new Date(b.date) - new Date(a.date),
+      render: (_, {created_at, status}) => {
+        const date = DateTime.fromISO(created_at);
+        const color = getStatusColor(status);
+        return (
+          <Tag color={color}>
+            {date.toLocaleString(DateTime.DATE_MED)}
+          </Tag>
+        )
+      }
+    },
+    {
+      title: 'Updated At',
+      dataIndex: 'updated_at',
+      key: 'job_id',
+      sorter: (a, b) => new Date(b.date) - new Date(a.date),
+      render: (_, {updated_at, status}) => {
+        const date = DateTime.fromISO(updated_at);
+        const color = getStatusColor(status);
+        return (
+          <Tag color={color}>
+            {date.toLocaleString(DateTime.DATE_MED)}
+          </Tag>
+        )
+      }
+    },
+    {
+      title: 'Lock Status',
+      dataIndex: 'locked',
+      key: 'job_id',
+      sorter: (a, b) => new Date(b.date) - new Date(a.date),
+      render: (_, {locked}) => {
+        const color = locked ? 'red' : 'green'
+        const displayText = locked ? 'LOCKED' : 'FREE'
+        return (
+          <Tag color={color}>
+            {displayText}
+          </Tag>
+        )
+      }
+    },
   ];
 
-  return <Table dataSource={jobs} columns={columns} pagination={{position: ['topLeft', 'bottomRight'],}} size={"middle"} sticky={true} scroll={{ y: 300, x: '100vw' }} />;
+  return <Table dataSource={jobs} columns={columns} pagination={{position: ['topLeft', 'bottomRight'],}} size={"small"} sticky={true} scroll={{ y: 300, x: '100vw' }} />;
 }
 
 export default CrawlQueueTableComponent
